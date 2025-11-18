a321a8a5c9903977b932d0b79c768a7542a77eed
rodriguesweslley897-debug <rodriguesweslley897@gmail.com>
2025-11-18 16:52:02 +0000

    atribuição

---
+++ atribuição.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+Atribuição Simples (=)
+Atribuição com Soma (+=)
+Atribuição com Subtração (-=)
+Atribuição com Multiplicação (*=)
+Atribuição com divisao (/=)
+*/
+
+int numero1 =10, numero2, resultado;
+
+resultado = 10;
+printf("Resultado: %d\n", resultado);
+
+resultado += 20;
+printf("Resultado %d\n", resultado);
+
+resultado -= numero1;
+printf("Resultado %d\n", resultado);
+
+resultado *= 7;
+printf("Resultado %d\n", resultado);
+
+resultado /= 4;
+printf("Resultado %d\n", resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+}
