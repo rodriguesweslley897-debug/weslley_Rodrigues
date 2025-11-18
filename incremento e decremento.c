7b4aa95ea258e908fae70d7baa63b3541f502a0d
rodriguesweslley897-debug <rodriguesweslley897@gmail.com>
2025-11-18 17:34:14 +0000

    incremento

---
+++ incremento e decremento.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+Incremento (++)
+Pré-Incremento ++a
+Pós-Incremento a++
+Decremento(--)
+Pré-Decremento --a
+Pós-Decremento a--
+*/
+
+int numero1 = 3;
+numero1 ++;
+printf("Antes incremento %d\n", numero1);
+
+printf("Apos incremento %d\n", numero1);
+
+numero1 --;
+printf("Apos decremento %d\n", numero1);
+
+
+
+
+
+
+
+
+
+}
